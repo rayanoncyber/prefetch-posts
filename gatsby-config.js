0b1160832b25d54c97a24acc4acf383a2ec32bf4
iamsupercharged <rayan@supercharged.gg>
2020-05-13 13:04:38 +0000

    First and last commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,22 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-firestore',
+      options: {
+        credential: require("./firebase.json"),
+        types: [
+          {
+            type: 'Post',
+            collection: 'posts',
+            map: doc => ({
+              title: doc.title,
+              text: doc.text
+            })
+          }
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,68 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import styled from 'styled-components';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+export const query = graphql`
+{
+    allPost {
+      edges {
+        node {
+          title
+          text
+          id
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({data}) => {
+  console.log(data)
+  
+  return <Layout>
+    {
+      data.allPost.edges.map(({node}) => (
+        <Card> 
+          <Content>
+            <Title> {node.title}</Title>
+            <Text> {node.text}</Text>
+          </Content>
+        </Card>
+      ))
+    }
   </Layout>
-)
+}
+
+const Card = styled.div`
+    width:100%;
+    min-height:100px;
+    padding: 30px 20px 10px 20px;
+    box-shadow: 0px 1px 1px rgba(10,10,10,0.1), 0px 1px 2px rgba(10,10,10,0.1), 0px 0px 1px rgba(10,10,10,0.8);
+    border-radius: 3px;
+    margin-bottom: 12px;
+    background: white;
+    cursor: pointer;
+    font-family: Roboto;
+    flex:1;
+    display: flex;
+`
+
+const Content = styled.div`
+  flex: 20;
+  padding: 0 20px;
+`
+
+const Title = styled.h1`
+    font-size: 32px;
+    margin: 0 0 14px 0;
+    color: black;
+`
+
+const Text = styled.p`
+    font-size: 18px;
+    color: black;
+`
 
 export default IndexPage
